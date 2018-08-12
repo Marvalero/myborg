2ff4ea0b94fa623901fbf1fdadfa8f2d469eeef0
Marvalero <chicemav@gmail.com>
2018-08-12 13:55:29 +0100

    Add bash completition

---
+++ cmd/completition.go
@@ -0,0 +1,25 @@
+package cmd
+
+import (
+	"os"
+
+	"github.com/spf13/cobra"
+)
+
+var completitionCmd = &cobra.Command{
+	Use:   "completition",
+	Short: "A brief description of your command",
+	Long: `A longer description that spans multiple lines and likely contains examples
+and usage of using your command. For example:
+
+Cobra is a CLI library for Go that empowers applications.
+This application is a tool to generate the needed files
+to quickly create a Cobra application.`,
+	Run: func(cmd *cobra.Command, args []string) {
+		rootCmd.GenBashCompletion(os.Stdout)
+	},
+}
+
+func init() {
+	rootCmd.AddCommand(completitionCmd)
+}
